4a0261f69a31b002b8557c550a56bb0bcafaeff3
Alex <alex150Rus@outlook.com>
2019-02-16 10:34:37 +0300

    Active records for working with db were created

---
+++ models/tables/Users.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace app\models\tables;
+
+use Yii;
+
+/**
+ * This is the model class for table "users".
+ *
+ * @property int $id
+ * @property string $login
+ * @property string $password
+ * @property string $email
+ */
+class Users extends \yii\db\ActiveRecord
+{
+    CONST SCENARIO_AUTH = 'auth';
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'users';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['login', 'password'], 'required'],
+            [['login', 'password'], 'string', 'max' => 75],
+            [['email'], 'string', 'max' => 255],
+        ];
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'login' => 'Login',
+            'password' => 'Password',
+            'email' => 'Email',
+        ];
+    }
+
+    public function fields()
+    {
+        if ($this->scenario == static::SCENARIO_AUTH) {
+          return [
+            'id',
+            'password',
+            'username' => 'login',
+          ];
+        }
+        return parent::fields();
+    }
+
+}
